22dfb3eef597c0a20b496aca636a83142c053d5f
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-02-12 21:48:36 +0530

    Create 2477. Minimum Fuel Cost to Report to the Capital.cpp

---
+++ 2477. Minimum Fuel Cost to Report to the Capital.cpp
@@ -0,0 +1,107 @@
+void __print(int x) { cout << x; }
+void __print(long long x) { cout << x; }
+void __print(unsigned long long x) { cout << x; }
+void __print(long double x) { cout << x; }
+void __print(char x) { cout << x; }
+void __print(const char *x) { cout << x; }
+void __print(const string &x) { cout << x; }
+void __print(bool x) { cout << (x ? "true" : "false"); }
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {
+	cout << '{';
+	__print(x.first);
+	cout << ", ";
+	__print(x.second);
+	cout << '}';
+}
+template<typename T>
+void __print(const T &x) {
+	int f = 0;
+	cout << '{';
+	for (auto &i : x) {
+		cout << (f++ ? ", " : "");
+		__print(i);
+	}
+	cout << "}";
+}
+void _print() {
+	cout << "]\n";
+}
+template<typename T, typename... V>
+void _print(T t, V... v) {
+	__print(t);
+	if (sizeof...(v)) {
+		cout << ", ";
+	}
+	_print(v...);
+}
+
+#define watch(x...) cout << "[" << #x << "] = ["; _print(x)
+#define ll 				  long long
+#define lld               long double
+#define INF               1000000000000000000
+#define pb                push_back
+#define all(x)            x.begin(),x.end()
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define sz(x)             (int)((x).size())
+#define f1                first
+#define s2                second
+#define fix(x)            fixed << setprecision(x)
+#define endl              "\n"
+#define bigprime          1000000007
+#define MOD               1000000007
+#define gcd(a,b)          __gcd((a),(b))
+#define lcm(a,b)          (a/gcd(a,b))*b
+#define scanvec(a,n)      rep(i,0,n){cin>>a[i];}
+#define fast              ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define makeuniq(v)       v.resize(unique(all(v)) - v.begin());
+
+/*
+#define int long long 
+#undef int 
+*/
+class Solution {
+public:
+    int ceilval(int a, int b) {return ((a / b) + ((a % b) != 0));}
+    ll ans = 0;
+    pair<int,int> dfs(vector<vector<int>> &adj,int curr,int seats,vector<int>&visited){
+        
+        
+        visited[curr] = 1;
+        
+        // cars and people
+        pair<int,int> Here = {1,1};
+        
+        for(auto x:adj[curr]){    
+            if(!visited[x]){
+               pair<int,int> child = dfs(adj,x,seats,visited);
+                Here.f1 += child.f1;
+                Here.s2 += child.s2;
+            }
+        }
+        
+        // Here we have cars and number of people 
+        // watch(curr);
+        // watch(Here);
+        int carstoSend = ceilval(Here.s2,seats);
+        if(curr != 0)
+            ans += carstoSend;
+        return {carstoSend,Here.s2};
+        
+    }
+    
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = sz(roads);
+        vector<vector<int>> adj(n+1,vector<int>());
+        vector<int> visited(n+1);
+        
+        for(auto x:roads){
+            adj[x[0]].pb(x[1]);
+            adj[x[1]].pb(x[0]);
+        }
+        
+        dfs(adj,0,seats,visited);
+        
+        return ans;
+    }
+};

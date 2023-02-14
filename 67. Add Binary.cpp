bca1ce82219362a7d1f10455d82ffcf0ad1e57fd
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-02-14 12:27:38 +0530

    Create 67. Add Binary.cpp

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,115 @@
+void __print(int x) { cout << x; }
+void __print(long long x) { cout << x; }
+void __print(unsigned long long x) { cout << x; }
+void __print(long double x) { cout << x; }
+void __print(char x) { cout << x; }
+void __print(const char *x) { cout << x; }
+void __print(const string &x) { cout << x; }
+void __print(bool x) { cout << (x ? "true" : "false"); }
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {
+    cout << '{';
+    __print(x.first);
+    cout << ", ";
+    __print(x.second);
+    cout << '}';
+}
+template<typename T>
+void __print(const T &x) {
+    int f = 0;
+    cout << '{';
+    for (auto &i : x) {
+        cout << (f++ ? ", " : "");
+        __print(i);
+    }
+    cout << "}";
+}
+void _print() {
+    cout << "]\n";
+}
+template<typename T, typename... V>
+void _print(T t, V... v) {
+    __print(t);
+    if (sizeof...(v)) {
+        cout << ", ";
+    }
+    _print(v...);
+}
+
+#define watch(x...) cout << "[" << #x << "] = ["; _print(x)
+#define lld               long double
+#define INF               1000000000000000000
+#define pb                push_back
+#define all(x)            x.begin(),x.end()
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define sz(x)             (int)((x).size())
+#define f1                first
+#define s2                second
+#define fix(x)            fixed << setprecision(x)
+#define endl              "\n"
+#define bigprime          1000000007
+#define MOD               1000000007
+#define gcd(a,b)          __gcd((a),(b))
+#define lcm(a,b)          (a/gcd(a,b))*b
+#define scanvec(a,n)      rep(i,0,n){cin>>a[i];}
+#define fast              ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define makeuniq(v)       v.resize(unique(all(v)) - v.begin());
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(all(a));
+        reverse(all(b));
+        int n = sz(a);
+        int m = sz(b);
+        int diff = abs(n-m);
+        if(n<m){
+            rep(i,0,diff){
+                a.pb('0');
+            }
+        }
+        if(m<n){
+            rep(i,0,diff){
+                b.pb('0');
+            }
+        }
+        n = max(n,m);
+        reverse(all(a));
+        reverse(all(b));
+        watch(a);
+        watch(b);
+        int carry = 0;
+        string ans = "";
+        for(int i = n-1;i>=0;i--){
+            if(a[i] == '1' && b[i] == '1' && carry == 1){
+                carry = 1;
+                ans.pb('1');
+            }else if(a[i] == '1' && b[i] == '1' && carry == 0){
+                carry = 1;
+                ans.pb('0');
+            }else if(a[i] == '1' && b[i] == '0' && carry == 1){
+                carry = 1;
+                ans.pb('0');
+            }else if(a[i] == '1' && b[i] == '0' && carry == 0){
+                carry = 0;
+                ans.pb('1');
+            }else if(a[i] == '0' && b[i] == '1' && carry == 1){
+                carry = 1;
+                ans.pb('0');
+            }else if(a[i] == '0' && b[i] == '1' && carry == 0){
+                carry = 0;
+                ans.pb('1');
+            }else if(a[i] == '0' && b[i] == '0' && carry == 1){
+                carry = 0;
+                ans.pb('1');
+            }else if(a[i] == '0' && b[i] == '0' && carry == 0){
+                carry = 0;
+                ans.pb('0');
+            }
+        }
+        if(carry){
+            ans.pb('1');
+        }
+        reverse(all(ans));
+        return ans;
+    }
+};

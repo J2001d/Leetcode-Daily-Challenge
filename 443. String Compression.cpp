3f70c1f20be30e132c10ab2aeb1e8a5c0f95450d
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-03-02 08:39:02 +0530

    Create 443. String Compression.cpp

---
+++ 443. String Compression.cpp
@@ -0,0 +1,100 @@
+ void __print(int x) { cout << x; }
+void __print(long long x) { cout << x; }
+void __print(unsigned long long x) { cout << x; }
+void __print(long double x) { cout << x; }
+void __print(char x) { cout << x; }
+void __print(const char *x) { cout << x; }
+void __print(const string &x) { cout << x; }
+void __print(bool x) { cout << (x ? "true" : "false"); }
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {
+	cout << '{';
+	__print(x.first);
+	cout << ", ";
+	__print(x.second);
+	cout << '}';
+}
+template<typename T>
+void __print(const T &x) {
+	int f = 0;
+	cout << '{';
+	for (auto &i : x) {
+		cout << (f++ ? ", " : "");
+		__print(i);
+	}
+	cout << "}";
+}
+void _print() {
+	cout << "]\n";
+}
+template<typename T, typename... V>
+void _print(T t, V... v) {
+	__print(t);
+	if (sizeof...(v)) {
+		cout << ", ";
+	}
+	_print(v...);
+}
+
+#define watch(x...) cout << "[" << #x << "] = ["; _print(x)
+#define ll 				  long long
+#define lld               long double
+#define INF               1000000000000000000
+#define pb                push_back
+#define all(x)            x.begin(),x.end()
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define sz(x)             (int)((x).size())
+#define f1                first
+#define s2                second
+#define fix(x)            fixed << setprecision(x)
+#define endl              "\n"
+#define bigprime          1000000007
+#define MOD               1000000007
+#define gcd(a,b)          __gcd((a),(b))
+#define lcm(a,b)          (a/gcd(a,b))*b
+#define scanvec(a,n)      rep(i,0,n){cin>>a[i];}
+#define fast              ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define makeuniq(v)       v.resize(unique(all(v)) - v.begin());
+
+/*
+#define int long long 
+#undef int 
+*/
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = sz(chars);
+        int cnt = 1;
+        
+        string here = "";
+        rep(i,0,n-1){
+            if(chars[i] == chars[i+1]){
+                cnt++;
+            }else{
+                if(cnt == 1){
+                    here.pb(chars[i]);
+                }else{
+                    here.pb(chars[i]);
+                    string number = to_string(cnt);
+                    here += number;
+                }
+                cnt = 1;
+            }
+            
+        }
+        if(cnt == 1){
+            here.pb(chars[n-1]);
+        }else{
+            here.pb(chars[n-1]);
+            string number = to_string(cnt);
+            here += number;
+        }
+        int i = 0;
+        for(auto x:here){
+            chars[i++] = x;
+        }
+        
+        return sz(here);
+        
+    }
+};

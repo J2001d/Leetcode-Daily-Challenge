f3abacb2c8007e5263ca873a98191d4407eb0b36
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-03-16 13:17:53 +0530

    Create 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp

---
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,115 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void __print(int x) { cout << x; }
+void __print(long long x) { cout << x; }
+void __print(unsigned long long x) { cout << x; }
+void __print(long double x) { cout << x; }
+void __print(char x) { cout << x; }
+void __print(const char *x) { cout << x; }
+void __print(const string &x) { cout << x; }
+void __print(bool x) { cout << (x ? "true" : "false"); }
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {
+	cout << '{';
+	__print(x.first);
+	cout << ", ";
+	__print(x.second);
+	cout << '}';
+}
+template<typename T>
+void __print(const T &x) {
+	int f = 0;
+	cout << '{';
+	for (auto &i : x) {
+		cout << (f++ ? ", " : "");
+		__print(i);
+	}
+	cout << "}";
+}
+void _print() {
+	cout << "]\n";
+}
+template<typename T, typename... V>
+void _print(T t, V... v) {
+	__print(t);
+	if (sizeof...(v)) {
+		cout << ", ";
+	}
+	_print(v...);
+}
+
+#define watch(x...) cout << "[" << #x << "] = ["; _print(x)
+#define ll 				  long long
+#define lld               long double
+#define INF               1000000000000000000
+#define pb                push_back
+#define all(x)            x.begin(),x.end()
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define sz(x)             (int)((x).size())
+#define f1                first
+#define s2                second
+#define fix(x)            fixed << setprecision(x)
+#define endl              "\n"
+#define bigprime          1000000007
+#define MOD               1000000007
+#define gcd(a,b)          __gcd((a),(b))
+#define lcm(a,b)          (a/gcd(a,b))*b
+#define scanvec(a,n)      rep(i,0,n){cin>>a[i];}
+#define fast              ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define makeuniq(v)       v.resize(unique(all(v)) - v.begin());
+
+/*
+#define int long long 
+#undef int 
+*/
+class Solution {
+public:
+    
+    TreeNode* helper(vector<int> &inorder,vector<int> &postorder,int instart,int inend,int poststart,int postend){
+        
+        if(instart > inend){
+            return NULL;
+        }
+        
+        TreeNode *root =new TreeNode(postorder[postend]);
+        
+        int i = instart;
+        
+        while(i <= inend){
+            if(inorder[i] == root->val){
+                break;
+            }
+            i++;
+        }
+        
+        int leftsize = i - instart;
+        int rightsize = inend - i;
+        
+        
+        root->left = helper(inorder,postorder,instart,i-1,poststart,poststart +leftsize-1);
+            
+        root->right = helper(inorder,postorder,i+1,inend,postend-rightsize,postend-1);
+            
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+        int n = sz(inorder);
+        int instart = 0,inend = n-1;
+        int poststart = 0,postend = n-1;
+        return helper(inorder,postorder,instart,inend,poststart,postend);
+        
+    }
+};
